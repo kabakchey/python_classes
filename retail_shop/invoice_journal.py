9349f1f83ebf70a6c20d14c436fa62cb150f6540
Dmitriy <dbhost@mail.ru>
2017-08-26 16:08:11 +0300

    Update invoice_journal.py

--- retail_shop/invoice_journal.py
+++ retail_shop/invoice_journal.py
@@ -20,9 +20,8 @@ class InvoiceJournal(object):
     def get_invoices(self, date1=None, date2=None):
         self.__invoices.sort(key=lambda inv : inv.date_time)
 
-        lower_limit = date1 if date1 != None else datetime.datetime.min
-        upper_limit = date2 if date2 != None else datetime.datetime.max
+        lower_limit = date1 if date1 not is None else datetime.datetime.min
+        upper_limit = date2 if date2 not is None else datetime.datetime.max
 
         return [invoice for invoice in self.__invoices if
                             lower_limit <= invoice.date_time <= upper_limit]
-ss
